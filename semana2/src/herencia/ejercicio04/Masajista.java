f97ec4cde712c372d112d00723ec537b8331ce41
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-01-23 10:54:48 +0100

    format

--- semana2/src/herencia/ejercicio04/Masajista.java
+++ semana2/src/herencia/ejercicio04/Masajista.java
@@ -1,28 +1,51 @@
 package herencia.ejercicio04;
+
 /**
  * La clase Masajista (Subclase de Persona)
+ * 
  * @author e.a.martin.muriel
  *
  */
-public class Masajista extends Persona{
+public class Masajista extends Persona {
 	private String titulacion;
 	private int aniosExperiencia;
-	
+
 	public Masajista() {
 		super();
 	}
+
 	public Masajista(String titulacion, int aExperiencia) {
 		super();
-		this.titulacion=titulacion;
-		this.aniosExperiencia=aExperiencia;
+		this.titulacion = titulacion;
+		this.aniosExperiencia = aExperiencia;
 	}
-	public Masajista(int id, String nombre, String apellidos, int edad,String titulacion, int aExperiencia) {
+
+	public Masajista(int id, String nombre, String apellidos, int edad, String titulacion, int aExperiencia) {
 		super(id, nombre, apellidos, edad);
-		this.titulacion=titulacion;
-		this.aniosExperiencia=aExperiencia;
+		this.titulacion = titulacion;
+		this.aniosExperiencia = aExperiencia;
 	}
-	
+
+	// Getters-setters
+	public String getTitulacion() {
+		return titulacion;
+	}
+
+	public void setTitulacion(String titulacion) {
+		this.titulacion = titulacion;
+	}
+
+	public int getAniosExperiencia() {
+		return aniosExperiencia;
+	}
+
+	public void setAniosExperiencia(int aniosExperiencia) {
+		this.aniosExperiencia = aniosExperiencia;
+	}
+
+	// Métodos
 	public void darMasaje() {
-		System.out.println("Está dando masaje");
+		System.out.println("Dando masaje");
 	}
+
 }
