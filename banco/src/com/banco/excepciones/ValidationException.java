485fa807556d62701108ba11ea9549f9fb4cd2a6
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-01-25 14:58:26 +0100

    Excepciones

--- banco/src/com/banco/excepciones/ValidationException.java
+++ banco/src/com/banco/excepciones/ValidationException.java
@@ -1,4 +1,5 @@
 package com.banco.excepciones;
+
 /**
  * Esta clas excepciones lanzará excepciones personalizadas
  * 
@@ -7,6 +8,20 @@ package com.banco.excepciones;
  */
 public class ValidationException extends RuntimeException {
 
-	
-	
+	String message;
+
+	public ValidationException() {
+		this.message = "Ha ocurrido una excepción de tipo ValidationException.";
+
+	}
+
+	public ValidationException(String message) {
+		this.message = message;
+
+	}
+
+	public void show() {
+		System.out.println("ValidationException Error: " + this.message);
+	}
+
 }
--- banco/srctest/com/banco/filtros/FiltroTest.java
+++ banco/srctest/com/banco/filtros/FiltroTest.java
@@ -9,8 +9,9 @@ import org.junit.jupiter.api.Test;
 class FiltroTest {
 	String cadPrueba = "Esto es una cadena de chorricientos carateres para pasar test";
 	LocalDate fActual = LocalDate.now();
-	LocalDate fAnterior= LocalDate.of(2022, 11, 23); 
-	LocalDate fPosterior= LocalDate.of(2024, 11, 23);
+	LocalDate fAnterior = LocalDate.of(2022, 11, 23);
+	LocalDate fPosterior = LocalDate.of(2024, 11, 23);
+
 	@Test
 	void testminLength1() {
 
@@ -22,35 +23,77 @@ class FiltroTest {
 
 		assertTrue(Filtro.minLength("1234", 2));
 	}
+
 	@Test
 	void testminLength3() {
 
 		assertFalse(Filtro.minLength("1234", 10));
 	}
-	
+
 	@Test
 	void testMaxLength1() {
 
 		assertTrue(Filtro.maxLength("1234", 10));
 	}
-	
+
 	@Test
 	void testMaxLength2() {
 
 		assertFalse(Filtro.maxLength("12345783", 4));
 	}
-	
+
 	@Test
 	void testFechaMaxima1() {
-		
-		assertTrue(Filtro.fechaMaxima(fActual,fPosterior));
+
+		assertTrue(Filtro.fechaMaxima(fActual, fPosterior));
 	}
+
 	@Test
 	void testFechaMaxima2() {
 
 		assertFalse(Filtro.fechaMaxima(fActual, fAnterior));
 	}
 
+	@Test
+	void testFechaValida1() {
 
-	
+		assertTrue(Filtro.fechaValida(fActual, fAnterior, fPosterior));
+	}
+
+	@Test
+	void testFechaValida2() {
+
+		assertTrue(Filtro.fechaValida(fActual, fActual, fPosterior));
+	}
+
+	@Test
+	void testFechaValida3() {
+
+		assertTrue(Filtro.fechaValida(fActual, fAnterior, fActual));
+	}
+
+	@Test
+	void testFechaValida4() {
+
+		assertTrue(Filtro.fechaValida(fActual, fActual, fActual));
+	}
+
+	@Test
+	void testFechaValida5() {
+
+		assertFalse(Filtro.fechaValida(fAnterior, fActual, fPosterior));
+	}
+
+	@Test
+	void testlongitudValida1() {
+
+		assertTrue(Filtro.longitudValida(cadPrueba, 10, 100));
+	}
+
+	@Test
+	void testlongitudValida2() {
+
+		assertFalse(Filtro.longitudValida(cadPrueba, 10, 15));
+	}
+
 }
