6e8398db0aacbf10ca1802125bd5f05d9bed7e9c
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-01-25 15:51:13 +0100

    test

---
+++ banco/srctest/com/banco/clases/CuentaTest.java
@@ -0,0 +1,79 @@
+package com.banco.clases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	@Test
+	void testCuentaStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMovimientos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMovimientos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNumero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNumero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTitular() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTitular() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMovimiento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSaldo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIngresarDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIngresarStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetirarDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetirarStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
--- banco/srctest/com/banco/clases/MovimientoTest.java
+++ banco/srctest/com/banco/clases/MovimientoTest.java
@@ -7,7 +7,42 @@ import org.junit.jupiter.api.Test;
 class MovimientoTest {
 
 	@Test
-	void test() {
+	void testMovimientoStringLocalDateDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetConcepto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetConcepto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetImporte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetImporte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
 		fail("Not yet implemented");
 	}
 
