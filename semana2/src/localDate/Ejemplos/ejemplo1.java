5651852483d2ef5668797616aded589cac4483ce
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-01-27 10:41:04 +0100

    cambios fecha

--- semana2/src/localDate/Ejemplos/ejemplo1.java
+++ semana2/src/localDate/Ejemplos/ejemplo1.java
@@ -2,38 +2,47 @@ package localDate.Ejemplos;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
 public class ejemplo1 {
 
 	public static void main(String[] args) {
-		
-		String fecha  = "1986-11-21";    //Ok
-		String fecha2  = "90-3-12";     //
-		String fecha3  = "15-04-1986"; //Si
-		String fecha4  = "20-Enero-20"; //no
-		String fecha5  = "dd/MM/yy";
-		String fecha6  = "dd/MM/yy";
+
+		String fecha = "1986-11-21"; // Ok
+		String fecha2 = "90-3-12"; //
+		String fecha3 = "15-04-1986"; // Si
+		String fecha4 = "20-Enero-20"; // no
 		String formato1 = "yyyy-mm-dd";
 		String formato2 = "dd/MM/yy";
+
+		LocalDate f1 = validaFormatoFecha(fecha, formato1);
+		LocalDate f2 = validaFormatoFecha(fecha3);
 		
-		
-		System.out.println(validaFormatoFecha(fecha, formato1).toString());
-		//System.out.println(Optional.ofNullable(validaFormatoFecha(fecha2, formato1).toString()).orElse("No es un formato de fecha correcto"));
-		
-		System.out.println("".toString());
+
+//		System.out.println(validaFormatoFecha(fecha, formato1));
+//		System.out.println(validaFormatoFecha(fecha3).toString());
+		// System.out.println(validaFormatoFecha(fecha2, formato1));
+//		System.out.println(validaFormatoFecha(fecha, formato1).toString());
+//		System.out.println(validaFormatoFecha(fecha, formato1).toString());
 
 	}
-	public static LocalDate validaFormatoFecha (String fecha, String formato) {
-		
-	
-		DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern(formato);
-		
-		//return LocalDate.parse(fecha,formatoFecha);//.format(formatoFecha);//.format(formatoFecha);
-		LocalDate fechaValida = LocalDate.parse(fecha,formatoFecha);
-		return fechaValida;
+
+	public static LocalDate validaFormatoFecha(String fecha, String formato) {
+		// Optional<LocalDate> fechaValida = Optional.empty(); //Solucion con Optional
+
+		try {
+			DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern(formato);
+			LocalDate fechaNueva = LocalDate.parse(fecha, formatoFecha);
+			System.out.println(fechaNueva.toString());
+			return fechaNueva;
+		} catch (DateTimeParseException e) {
+			
+		}
+		return null;
 	}
-	public static LocalDate validaFormatoFecha (String  fecha) {
-		return validaFormatoFecha ( fecha, "dd-mm-YYYY");
+
+	public static LocalDate validaFormatoFecha(String fecha) {
+		return validaFormatoFecha(fecha, "dd-mm-YYYY");
 	}
 }
