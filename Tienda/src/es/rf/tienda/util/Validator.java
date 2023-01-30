579291c7c8dae8ea013f0d68da3af6434ee79533
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-01-30 16:31:56 +0100

    Rename project

---
+++ Tienda/src/es/rf/tienda/util/Validator.java
@@ -0,0 +1,372 @@
+package es.rf.tienda.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/********************************************************************************************
+ * NOMBRE: Validator.java
+ * 
+ * DESCRIPCION: Clase auxiliar para validar los datos que sean introducidos en
+ * la aplicaci�n.
+ * 
+ * @version 30/01/2023
+ * @author Elisabet Martin
+ * 
+ ******************************************************************************************/
+public class Validator {
+
+	
+	private static final String CODPRODUCTO_PATTERN = "^[A-Z]{2}[\\d]{3}+$";
+	private static final String ALFANUMERIC_PATTERN = "^[0-9a-zA-Z]+$";
+
+	private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+	/**
+	 * Patr�n para validar el email, evitando puntos finales antes de la @ y que
+	 * solo contenga car�cteres alfanum�ricos
+	 */
+	private final static String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	/**
+	 * Permite verificar que un DNI cumple con el patr�n XX.XXX.XXX-L
+	 */
+	private final static String DNI_PATTERN = "\\d{2}\\.\\d{3}\\.\\d{3}-[a-zA-Z]";
+
+	/**
+	 * Permite validar un tel�fono, el cual debe contener de 10 a 20 d�gitos y donde
+	 * los espacios est�n permitidos
+	 */
+	private final static String PHONE_PATTERN = "[\\d ]{10,20}";
+
+	/**
+	 * Orden de las letras con las cuales se comprobar� la validez del DNI
+	 */
+	private final static String LETRA_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+	/**
+	 * Longitud que debe tener todo DNI pasado a la aplicaci�n.
+	 */
+	private final static int LONGITUD_DNI = 12;
+
+	/*
+	 * *****************************************************************************
+	 * ********** NOMBRE: isAlfanumeric *
+	 * 
+	 * DESCRIPCI�N:
+	 *//**
+		 * Permite verificar que el texto pasado solo contiene caracters alfanum�ricos
+		 * 
+		 * @param texto String a verificar que solo tenga car�cteres alfanum�ricos
+		 * 
+		 * @return true, si cumple solo contiene caracters alfanum�ricos. <br>
+		 *         false en caso contrario FECHA: Enero 2023
+		 * 
+		 *         AUTOR: Elisabet Martin
+		 * 
+		 **************************************************************************************/
+	public static boolean isAlfanumeric(String texto) {
+
+		Pattern pattern = Pattern.compile(ALFANUMERIC_PATTERN, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(texto);
+		return matcher.find();
+
+	}
+
+	/**
+	 * Metodo isVacio
+	 * 
+	 * @param prueba
+	 * @return boolean
+	 */
+
+	public static boolean isVacio(String prueba) {
+		return prueba == null || prueba.isEmpty();
+
+	}
+
+	/*
+	 * *****************************************************************************
+	 * ********** NOMBRE: cumplePhoneNumber *
+	 * 
+	 * DESCRIPCI�N:
+	 *//**
+		 * El phone number debe tener un total de entre 10 y 20, contando d�gitos y
+		 * espacios. M�nimo aceptable son 10 d�gitos.
+		 * 
+		 * @param phoneNumber String con el n�mero de telefono de entre 10 y 20 d�gitos.
+		 *                    Puede tener espacios, pero siempre con 10 d�gitos como
+		 *                    m�nimo.
+		 * 
+		 * @return true, si cumple todas las condiciones
+		 *
+		 *         FECHA: Enero 2023 AUTOR: Elisabet Martin
+		 * 
+		 **************************************************************************************/
+	public static boolean cumplePhoneNumber(String phoneNumber) {
+		Pattern pattern = Pattern.compile(PHONE_PATTERN, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(phoneNumber);
+		boolean valido = false;
+
+		if (matcher.find()) { // Cumple la expresión regular: blancos+digitos>10 && blancos+digitos<20
+
+			String soloDig = phoneNumber.replace(" ", "");
+			if (soloDig.length() >= 10) {
+				valido = true;
+			}
+		}
+
+		return valido;
+
+	}
+
+	/*
+	 * *****************************************************************************
+	 * ********** NOMBRE: isEmailValido *
+	 * 
+	 * DESCRIPCI�N:
+	 *//**
+		 * Comprueba si el email tiene un formato que pueda ser v�lido.
+		 * 
+		 * @param email String a comprobar
+		 * 
+		 * @return true, en caso que el formato sea v�lido FECHA: Enero 2023
+		 * 
+		 *         AUTOR: Elisabet Martin
+		 * 
+		 **************************************************************************************/
+	public static boolean isEmailValido(String email) {
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.find();
+	}
+
+	/*
+	 * *****************************************************************************
+	 * ********** NOMBRE: cumpleDNI *
+	 * 
+	 * DESCRIPCI�N:
+	 *//**
+		 * Esta funci�n verifica que el DNI cumple el siguiente formato: xx.xxx.xxx-L
+		 * <br>
+		 * El DNI ha de tener longitud 12
+		 * 
+		 * @param dni String con DNI a ser validado
+		 * 
+		 * @return true, si el DNI cumple el estandar nacional. FECHA: Enero 2023 AUTOR:
+		 *         Elisabet Martin
+		 * 
+		 **************************************************************************************/
+	public static boolean cumpleDNI(String dni) {
+		boolean valido = false;
+		int num;
+		String sNum = "";
+
+		if (dni.matches(DNI_PATTERN)) { // Primer filtro-> cumple patrón
+
+			for (int i = 0; i < LONGITUD_DNI - 2; i++) {
+				if (Character.isDigit(dni.charAt(i))) { // Estraer los numéricos
+					sNum += dni.charAt(i);
+				}
+			}
+
+			num = Integer.parseInt(sNum); // Cast a entero
+
+			if (LETRA_DNI.charAt(num % 23) == dni.charAt(LONGITUD_DNI - 1)) { // Compara la letra
+				valido = true;
+			}
+		}
+
+		return valido;
+
+	}
+
+	/**
+	 * ***************************************************************************************
+	 * NOMBRE: cumpleRango *
+	 * 
+	 * DESCRIPCI�N:
+	 */
+	/**
+	 * Comprueba que un N�mero se necuentra entre 2 valores
+	 * 
+	 * @param valor       (int)/(double) N�mero a comprobar
+	 * @param valorMinimo (int) N�mero valor aceptable
+	 * @param valorMaximo (int) M�N�mero valor aceptable
+	 * 
+	 * @return true si valorMinimo > valor > valorMaximo FECHA: Enero 2023 AUTOR:
+	 *         Elisabet Martin
+	 * 
+	 **************************************************************************************/
+	public static boolean cumpleRango(int valor, int valorMinimo, int valorMaximo) {
+
+		return valor >= valorMinimo && valor <= valorMaximo;
+
+	}
+
+	public static boolean cumpleRango(double valor, int valorMinimo, int valorMaximo) {
+
+		return cumpleRango((int) valor, valorMinimo, valorMaximo);
+
+	}
+
+	/*
+	 * *****************************************************************************
+	 * ********** NOMBRE: cumpleLongitudMin *
+	 * 
+	 * DESCRIPCI�N:
+	 *//**
+		 * Comprobar que el texto pasado tiene una longitud de al menos x caracteres,
+		 * siendo x el entero pasado como par�metro
+		 * 
+		 * @param texto          String texto a comprobar
+		 * @param longitudMinima int que indique longitud m�nima.
+		 * 
+		 * @return cierto, si la longitud del texto es mayor o igual que longitudMinima
+		 *         FECHA: Enero 2023 AUTOR: Elisabet Martin
+		 * 
+		 **************************************************************************************/
+	public static boolean cumpleLongitudMin(String texto, int longitudMinima) {
+
+		boolean cumple = false;
+		if (texto.length() >= longitudMinima)
+			cumple = true;
+		return cumple;
+
+	}
+
+	/*
+	 * *****************************************************************************
+	 * ********** NOMBRE: cumpleLongitudMax *
+	 * 
+	 * DESCRIPCI�N:
+	 *//**
+		 * Comprobar que el texto pasado tiene una longitud de, como mucho, x
+		 * caracteres, siendo x el entero pasado como par�metro
+		 * 
+		 * @param texto          String con el texto a comprobar
+		 * @param longitudMaxima int con la longitud m�xima del texto
+		 * 
+		 * @return true, si el texto es menor o igual que la longitud m�xima. FECHA:
+		 *         Enero 2023 AUTOR: Elisabet Martin
+		 * 
+		 **************************************************************************************/
+	public static boolean cumpleLongitudMax(String texto, int longitudMaxima) {
+		boolean cumple = false;
+		if (texto.length() <= longitudMaxima)
+			cumple = true;
+		return cumple;
+
+	}
+
+	/****************************************************************************************
+	 * NOMBRE: cumpleLongitud *
+	 * 
+	 * DESCRIPCI�N:
+	 */
+	/**
+	 * Comprobar que la longitud de un texto se encuentra entre unos valores m�ximos
+	 * y m�nimos
+	 * 
+	 * @param texto          String con el texto que a validar
+	 * @param longitudMinima (int) M�nima longitud del texto
+	 * @param longitudMaxima (int) M�xima longitud v�lida para el texo
+	 * 
+	 * @return true, si la longitud del texto cumple: longitudMinima <=
+	 *         longitudTexto <=longitudMaxima FECHA: Enero 2023 AUTOR: Elisabet
+	 *         Martin
+	 * 
+	 **************************************************************************************/
+	public static boolean cumpleLongitud(String texto, int longitudMinima, int longitudMaxima) {
+		boolean cumple = false;
+		if (cumpleLongitudMax(texto, longitudMaxima) && cumpleLongitudMin(texto, longitudMinima))
+			cumple = true;
+		return cumple;
+
+	}
+
+	/**
+	 * Valida una fecha calendar con m�nimo min
+	 * 
+	 * @param fecha
+	 * @param min
+	 * @return
+	 */
+
+	public static boolean valDateMin(LocalDate fecha, LocalDate min) {
+		boolean valido = false;
+		if (fecha.isAfter(min) || fecha.isEqual(min)) {
+			valido = true;
+		}
+		return valido;
+
+	}
+
+	/**
+	 * Valida una fecha calendar con m�ximo max
+	 * 
+	 * @param fecha
+	 * @param max
+	 * @return
+	 */
+	public static boolean valDateMax(LocalDate fecha, LocalDate max) {
+		boolean valido = false;
+		if (fecha.isBefore(max) || fecha.isEqual(max)) {
+			valido = true;
+		}
+		return valido;
+
+	}
+
+	/**
+	 * esFechaValida Recibe una string con formato fecha dd/mm/aaaa y comprueba el
+	 * formato
+	 * 
+	 * @param fecha
+	 * @return
+	 */
+	public static boolean esFechaValida(String fecha) {
+		boolean valido = true;
+		DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		try {
+			LocalDate fechaNueva = LocalDate.parse(fecha, formatoFecha);
+		} catch (DateTimeParseException e) {
+			valido = false;
+		}
+
+		return valido;
+
+	}
+
+	/**
+	 * Nombre esPasswordValida Descripcion Comprueba que la cadena recibida cumpla
+	 * con lasnormas de contrase�a
+	 * 
+	 * @param password string con la contrase�a introducida
+	 * @return true si cumple con las especificaciones
+	 * 
+	 *         Especificacion: Autor: Elisabet Martin
+	 */
+	public static boolean esPasswordValida(String password) {
+		Pattern pattern = Pattern.compile(PASSWORD_PATTERN, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.find();
+
+	}
+	
+	/**
+	 * El método esCodigoProductoValido. Comprueba que el código cumpla con el patrón. 
+	 * Patron: AA000 (5char: 2 Letras mayúsculas+3 digitos.
+	 * 
+	 * @param password string con el código de Producto
+	 * @return true si cumple con las especificaciones
+	 * 
+	 *         Especificacion: Autor: Elisabet Martin
+	 */
+	public static boolean esCodigoProductoValido(String codProducto) {
+		return codProducto.matches(CODPRODUCTO_PATTERN);
+
+	}
+
+}
---
+++ Tienda/srctest/es/rf/tienda/util/ValidatorTest.java
@@ -0,0 +1,386 @@
+package es.rf.tienda.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+
+	final String ALFANUMERICO_TRUE = "dfkdfkj2323232323";
+	final String ALFANUMERICO_FALSE = "\"dfkdfkj2232323-$\"";
+	final String STRING_VACIO_TRUE = "";
+	final String STRING_VACIO_TRUE_NULL = null;
+	final String STRING_VACIO_FALSE = "Esta cadena no está vacia";
+	final String TELEFONO_VALIDO1 = "616 11 11 170";
+	final String TELEFONO_VALIDO2 = "6161111170";
+	final String TELEFONO_VALIDO3 = "616 11 11 17345";
+	final String TELEFONO_ERR_NODIGITOS = "1234467ejm8910";
+	final String TELEFONO_ERR_CORTO = "123446789";
+	final String TELEFONO_ERR_CORTO2 = "1 2 3 4 5 6 7 8   ";
+	final String TELEFONO_ERR_LARGO = "1234467891012345678901234567890";
+	final String EMAIL_VALIDO1 = "foo@goo.com";
+	final String EMAIL_VALIDO2 = "foo.es@foo.es";
+	final String EMAIL_ERR1 = "foo.esfoo.es";
+	final String EMAIL_ERR2 = "@foo.es";
+	final String EMAIL_ERR3 = "foo.es@";
+	final String EMAIL_ERR4 = "@";
+	final String EMAIL_ERR5_NOPUNTO = "foo@goo";
+	final String EMAIL_ERR6_PUNTO = "foo@goo.";
+	final int INT_1 = 1;
+	final int INT_3 = 3;
+	final int INT_9 = 9;
+	final double DOUBLE_1 = 1.0;
+	final double DOUBLE_3 = 3.0;
+	final double DOUBLE_9 = 9.0;
+
+	final String DNI_VALIDO = "28.793.108-Y";
+	final String DNI_ERROR_PATRON = "28793108-Y";
+	final String DNI_ERROR_LETRA = "28.793.108-J";
+	final String CAD_5 = "Cinco";
+	final String CAD_1 = "Uno";
+	final String CAD_ERROR_LONGITUD = "Pero esta cadena de excede";
+	final String CAD_VACIA = "";
+	final String FECHA_VALIDA = "25/04/2005";
+	final String FECHA_ERROR1 = "25/04/05";
+	final String FECHA_ERROR2 = "25/4/05";
+	final String FECHA_ERROR3 = "25-04-2005";
+	final String FECHA_ERROR4 = "2005/05/04";
+
+	final String PWD_VALIDA = "Contra@VA123";
+	final String PWD_ERROR1_NO_CHAR_ESP = "ContraVA123";
+	final String PWD_ERROR2_NO_CAP = "contra_va123";
+	final String PWD_ERROR3_NO_DIG = "Contra_VAddsj";
+
+	final String CODPROD_VALIDO = "AA000";
+	final String CODPROD_ERROR1 = "Aa000";
+	final String CODPROD_ERROR2 = "A1239";
+	final String CODPROD_ERROR3 = "$A1239";
+	final String CODPROD_ERROR5_LARGO = "AA0000";
+	final String CODPROD_ERROR4_CORTO = "AA00";
+
+	LocalDate fecha1 = LocalDate.now();
+	LocalDate fecha2 = LocalDate.now().plusMonths(INT_3);
+	LocalDate fecha3 = LocalDate.now().plusMonths(INT_9);
+
+	@Test
+	void testIsAlfanumeric1() {
+
+		assertTrue(Validator.isAlfanumeric(ALFANUMERICO_TRUE));
+	}
+
+	@Test
+	void testIsAlfanumeric2() {
+		assertFalse(Validator.isAlfanumeric(ALFANUMERICO_FALSE));
+	}
+
+	@Test
+	void testIsVacio() {
+		assertTrue(Validator.isVacio(STRING_VACIO_TRUE));
+	}
+
+	@Test
+	void testIsVacio2() {
+		assertTrue(Validator.isVacio(STRING_VACIO_TRUE_NULL));
+	}
+
+	@Test
+	void testIsVacio3() {
+		assertFalse(Validator.isVacio(STRING_VACIO_FALSE));
+	}
+
+	@Test
+	void testCumplePhoneNumber1() {
+		assertTrue(Validator.cumplePhoneNumber(TELEFONO_VALIDO1));
+	}
+
+	@Test
+	void testCumplePhoneNumber2() {
+		assertTrue(Validator.cumplePhoneNumber(TELEFONO_VALIDO2));
+	}
+
+	@Test
+	void testCumplePhoneNumber3() {
+		assertTrue(Validator.cumplePhoneNumber(TELEFONO_VALIDO3));
+	}
+
+	@Test
+	void testCumplePhoneNumber4() {
+
+		assertFalse(Validator.cumplePhoneNumber(TELEFONO_ERR_NODIGITOS));
+	}
+
+	@Test
+	void testCumplePhoneNumber5() {
+
+		assertFalse(Validator.cumplePhoneNumber(TELEFONO_ERR_CORTO));
+	}
+
+	@Test
+	void testCumplePhoneNumber6() {
+
+		assertFalse(Validator.cumplePhoneNumber(TELEFONO_ERR_CORTO2));
+	}
+
+	@Test
+	void testIsEmailValido_1() {
+		assertTrue(Validator.isEmailValido(EMAIL_VALIDO1));
+	}
+
+	@Test
+	void testIsEmailValido_2() {
+		assertTrue(Validator.isEmailValido(EMAIL_VALIDO2));
+	}
+
+	@Test
+	void testIsEmailValido_3() {
+		assertFalse(Validator.isEmailValido(EMAIL_ERR1));
+	}
+
+	@Test
+	void testIsEmailValido_4() {
+		assertFalse(Validator.isEmailValido(EMAIL_ERR2));
+	}
+
+	@Test
+	void testIsEmailValido_5() {
+		assertFalse(Validator.isEmailValido(EMAIL_ERR3));
+	}
+
+	@Test
+	void testIsEmailValido_6() {
+		assertFalse(Validator.isEmailValido(EMAIL_ERR4));
+	}
+
+	@Test
+	void testIsEmailValido_7() {
+		assertFalse(Validator.isEmailValido(EMAIL_ERR5_NOPUNTO));
+	}
+
+	@Test
+	void testIsEmailValido_8() {
+		assertFalse(Validator.isEmailValido(EMAIL_ERR6_PUNTO));
+	}
+
+	@Test
+	void testCumpleDNI_True() {
+
+		assertTrue(Validator.cumpleDNI(DNI_VALIDO));
+		assertFalse(Validator.cumpleDNI(DNI_ERROR_LETRA));
+		assertFalse(Validator.cumpleDNI(DNI_ERROR_PATRON));
+	}
+
+	@Test
+	void testCumpleDNI_False() {
+
+		assertFalse(Validator.cumpleDNI(DNI_ERROR_LETRA));
+	}
+
+	@Test
+	void testCumpleDNI_False2() {
+
+		assertFalse(Validator.cumpleDNI(DNI_ERROR_PATRON));
+	}
+
+	@Test
+	void testCumpleRangoIntIntInt_1() {
+		assertTrue(Validator.cumpleRango(INT_3, INT_1, INT_9));
+	}
+
+	@Test
+	void testCumpleRangoIntIntInt_2() {
+		assertTrue(Validator.cumpleRango(INT_3, INT_3, INT_3));
+	}
+
+	@Test
+	void testCumpleRangoIntIntInt_3() {
+		assertFalse(Validator.cumpleRango(INT_1, INT_3, INT_9));
+	}
+
+	@Test
+	void testCumpleRangoIntIntInt_4() {
+		assertFalse(Validator.cumpleRango(INT_9, INT_1, INT_3));
+	}
+
+	@Test
+	void testCumpleRangoDoubleIntInt_1() {
+		assertTrue(Validator.cumpleRango(DOUBLE_3, INT_1, INT_9));
+	}
+
+	@Test
+	void testCumpleRangoDoubleIntInt_2() {
+		assertTrue(Validator.cumpleRango(DOUBLE_3, INT_3, INT_3));
+	}
+
+	@Test
+	void testCumpleRangoDoubleIntInt_3() {
+		assertFalse(Validator.cumpleRango(DOUBLE_1, INT_3, INT_9));
+	}
+
+	@Test
+	void testCumpleRangoDoubleIntInt_4() {
+		assertFalse(Validator.cumpleRango(DOUBLE_9, INT_1, INT_3));
+	}
+
+	@Test
+	void testCumpleLongitudMin_true1() {
+
+		assertTrue(Validator.cumpleLongitudMin(CAD_5, INT_3));
+
+	}
+
+	@Test
+	void testCumpleLongitudMin_true2() {
+
+		assertTrue(Validator.cumpleLongitudMin(CAD_1, INT_3));
+
+	}
+
+	@Test
+	void testCumpleLongitudMin_false() {
+
+		assertFalse(Validator.cumpleLongitudMin(CAD_1, INT_9));
+	}
+
+	@Test
+	void testCumpleLongitudMax() {
+
+		assertTrue(Validator.cumpleLongitudMax(CAD_5, INT_9));
+
+	}
+
+	@Test
+	void testCumpleLongitudMax_true2() {
+
+		assertTrue(Validator.cumpleLongitudMax(CAD_1, INT_3));
+
+	}
+
+	@Test
+	void testCumpleLongitudMax_false() {
+
+		assertFalse(Validator.cumpleLongitudMax(CAD_ERROR_LONGITUD, INT_9));
+	}
+
+	@Test
+	void testCumpleLongitud_true1() {
+
+		assertTrue(Validator.cumpleLongitud(CAD_5, INT_1, INT_9));
+
+	}
+
+	@Test
+	void testCumpleLongitud_true2() {
+
+		assertTrue(Validator.cumpleLongitud(CAD_1, INT_3, INT_3));
+
+	}
+
+	@Test
+	void testCumpleLongitud_false1() {
+
+		assertFalse(Validator.cumpleLongitud(CAD_ERROR_LONGITUD, INT_1, INT_9));
+
+	}
+
+	@Test
+	void testCumpleLongitud_false2() {
+
+		assertFalse(Validator.cumpleLongitud(CAD_VACIA, INT_1, INT_9));
+	}
+
+	@Test
+	void testValDateMin() {
+
+		assertTrue(Validator.valDateMin(fecha2, fecha1));
+	}
+
+	@Test
+	void testValDateMin_2() {
+		assertTrue(Validator.valDateMin(fecha2, fecha2));
+	}
+
+	@Test
+	void testValDateMin_3() {
+		assertFalse(Validator.valDateMin(fecha1, fecha2));
+	}
+
+	@Test
+	void testValDateMax() {
+		assertTrue(Validator.valDateMax(fecha1, fecha3));
+	}
+
+	@Test
+	void testValDateMax2() {
+		assertTrue(Validator.valDateMax(fecha2, fecha2));
+	}
+
+	@Test
+	void testValDateMax3() {
+		assertFalse(Validator.valDateMax(fecha2, fecha1));
+	}
+
+	@Test
+	void testEsFechaValida() {
+		assertTrue(Validator.esFechaValida(FECHA_VALIDA));
+	}
+
+	@Test
+	void testEsFechaValida2() {
+		assertTrue(Validator.esFechaValida(FECHA_VALIDA));
+	}
+
+	@Test
+	void testEsFechaValida3() {
+		assertFalse(Validator.esFechaValida(FECHA_ERROR1));
+	}
+
+	@Test
+	void testEsFechaValida4() {
+		assertFalse(Validator.esFechaValida(FECHA_ERROR2));
+	}
+
+	@Test
+	void testEsFechaValida5() {
+		assertFalse(Validator.esFechaValida(FECHA_ERROR3));
+	}
+
+	@Test
+	void testEsPasswordValida() {
+		assertTrue(Validator.esPasswordValida(PWD_VALIDA));
+	}
+
+	@Test
+	void testEsPasswordValida2() {
+		assertFalse(Validator.esFechaValida(PWD_ERROR1_NO_CHAR_ESP));
+	}
+
+	@Test
+	void testEsPasswordValida3() {
+		assertFalse(Validator.esFechaValida(PWD_ERROR2_NO_CAP));
+	}
+
+	@Test
+	void testEsPasswordValida4() {
+		assertFalse(Validator.esFechaValida(PWD_ERROR3_NO_DIG));
+	}
+
+	@Test
+	void testEsCodigoProductoValido1() {
+		assertTrue(Validator.esCodigoProductoValido(CODPROD_VALIDO));
+	}
+
+	@Test
+	void testEsCodigoProductoValido2() {
+		assertFalse(Validator.esCodigoProductoValido(CODPROD_ERROR1));
+		assertFalse(Validator.esCodigoProductoValido(CODPROD_ERROR2));
+		assertFalse(Validator.esCodigoProductoValido(CODPROD_ERROR3));
+		assertFalse(Validator.esCodigoProductoValido(CODPROD_ERROR4_CORTO));
+		assertFalse(Validator.esCodigoProductoValido(CODPROD_ERROR5_LARGO));
+
+	}
+
+}
