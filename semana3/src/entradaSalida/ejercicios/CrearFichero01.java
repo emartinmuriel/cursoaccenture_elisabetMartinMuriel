c45f5053e4293d9bea51c646f2a1998839e71a71
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-01-31 21:00:47 +0100

    Crear-Leer Fichero corregidos- todo ok

--- semana3/src/entradaSalida/ejercicios/CrearFichero01.java
+++ semana3/src/entradaSalida/ejercicios/CrearFichero01.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * 
  * NOMBRE: XXXXXXXXXXXX APELLIDO: yyyyyyyyyyyyy
  * 
- * Otra clase leeráeste fichero eimprimirá por pantalla el contenido.
+ * Otra clase leerá estefichero eimprimirá por pantalla el contenido.
  * 
  * @author e.a.martin.muriel
  *
@@ -20,8 +20,8 @@ import java.util.Scanner;
 public class CrearFichero01 {
 
 	public static void main(String[] args) {
-		
-		//Leer datos de teclado
+
+		// Leer datos de teclado
 		Scanner entradaConsola = new Scanner(System.in);
 		String nombre, apellido1;
 
@@ -33,37 +33,35 @@ public class CrearFichero01 {
 		apellido1 = entradaConsola.next();
 		entradaConsola.nextLine();
 
-		//Crear fichero
-		
+		// Crear fichero
+
 		FileWriter fichero = null;
 		PrintWriter pw = null;
 		String path = "C:\\Users\\e.a.martin.muriel\\Desktop\\FicherosJava1\\";
 
 		try {
-			fichero = new FileWriter(path + "NombreApellidos.txt"); // Abro el fichero para Escritura, si no existe lo crea.
-			
-			pw = new PrintWriter (fichero);  //Escribe en el fichero
-				
-				pw.println ("NOMBRE: " + nombre);    
-				pw.println ("APELLIDO: " + apellido1); 
-			
-			
+			fichero = new FileWriter(path + "NombreApellidos.txt"); // Abro el fichero para Escritura, si no existe lo
+																	// crea.
+
+			pw = new PrintWriter(fichero); // Escribe en el fichero
+
+			pw.println("NOMBRE=" + nombre + "\n");
+			pw.println("APELLIDO=" + apellido1);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (fichero != null) {	//se controla el error
-					fichero.close();	//Cerramso fichero
+				if (fichero != null) { // se controla el error
+					fichero.close(); // Cerramso fichero
 				}
 			} catch (IOException e2) {
 				e2.printStackTrace();
 			}
 		}
-		
-		
-		
-		//Lectura de fichero 
-		
+
+		// Lectura de fichero
+
 		LeerFichero01 miLector = new LeerFichero01();
 		miLector.imprimeFichero();
 	}
--- semana3/src/entradaSalida/ejercicios/LeerFichero01.java
+++ semana3/src/entradaSalida/ejercicios/LeerFichero01.java
@@ -1,26 +1,43 @@
 package entradaSalida.ejercicios;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class LeerFichero01 {
 
-	public void imprimeFichero()
-	{	
+	public void imprimeFichero() {
 		File fichero = new File("C:\\Users\\e.a.martin.muriel\\Desktop\\FicherosJava1\\NombreApellidos.txt");
-		try (Scanner lectorFichero = new Scanner(fichero)) { // Try con resultados automaticamente cierra el fichero
-																// cuando sale del try
-			lectorFichero.useDelimiter("\n"); 
-			String textoFichero="";
-			while (lectorFichero.hasNext()) {
-				textoFichero += lectorFichero.nextLine() + " ";
-			
+		try (FileReader lector = new FileReader(fichero); BufferedReader bf = new BufferedReader(lector)) { // Try con
+																											// resultados
+																											// automaticamente
+																											// cierra el
+																											// fichero
+			; // cuando sale del try
+			String textoFichero = "";
+			int numcar;
+			while ((numcar = lector.read()) != -1) { // Hasta fin de archivo
+				if ((char) numcar != '\n' && (char) numcar != '\r') {
+					textoFichero += (char) numcar;
+
+				} else if ((char) numcar == '\r') {
+					textoFichero += ("|");
+				}
+
 			}
-			System.out.println(textoFichero);
+			String texto = textoFichero.substring(0, textoFichero.length() - 1);
+			// Doble split
+			String[] arrClaves = texto.split("\\|");
+			for (String claveValor : arrClaves) {
+
+				String[] cVal = claveValor.split("=");
+				System.out.print(cVal[1] + " ");
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-}
+	}
 }
