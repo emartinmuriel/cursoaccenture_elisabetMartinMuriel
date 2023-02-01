1b5bbc06cfdbe87f594961c049d9940bd1fa3605
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-02-01 12:53:18 +0100

    Ejericicio Hilos. generar-presentar numeros version 1

---
+++ semana3/src/multitarea/ejercicios03/Main.java
@@ -0,0 +1,28 @@
+package multitarea.ejercicios03;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Buffer bf = new Buffer();
+		bf.setDisponible(false); // Inicializa el buffer
+
+		GeneraNumero genNum = new GeneraNumero();
+		genNum.setBuffer(bf);
+
+		PresentaNumero presNum = new PresentaNumero();
+		presNum.setBuffer(bf);
+
+		/*
+		 * Añadimos las clases sincronizadas a los hilos creados (pq implemnetan la
+		 * interfaz Runnable)
+		 */
+		Thread hilo1 = new Thread(genNum);
+		Thread hilo2 = new Thread(presNum);
+
+		hilo1.start(); // Lanzamos cada uno de los hilos
+		hilo2.start();
+	}
+
+}
