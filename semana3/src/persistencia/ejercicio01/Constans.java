292cbb63c724c02114c2d8cf202e304d013e847b
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-02-03 15:04:58 +0100

    Ejemplo de clase JDBC

---
+++ semana3/src/persistencia/ejercicio01/Constans.java
@@ -0,0 +1,10 @@
+package persistencia.ejercicio01;
+
+public class Constans {
+ public static final String USUARIO ="";
+ public static final String PWD ="";
+ public static final String DATABASE="";
+ public static final String JDBC_DRIVER ="oracle.jdbc.driver.OracleDriver";
+ public static final String URL_LOCAL ="jdbc:oracle:thin:@localhost:1521:xe";
+ public static final String URL ="jdbc:oracle:thin:@95.17.8.252:1521:xe";
+}
