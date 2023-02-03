05f5d1a9bca1abd6711ce28725d314bfcfc180a3
emartinmuriel <e.a.martin.muriel@accenture.com>
2023-02-03 15:04:32 +0100

    Avance Tienda

--- Tienda/src/es/rf/tienda/dominio/Categoria.java
+++ Tienda/src/es/rf/tienda/dominio/Categoria.java
@@ -1,40 +1,42 @@
 package es.rf.tienda.dominio;
 
+import es.rf.tienda.exception.DomainException;
 import es.rf.tienda.util.Validator;
 
 /**
  * 
- * Nombre		Categoria
- * Descripcion	Lista de categorías
- * @author 		Miguel Garcia
- * @version		13 de abr. de 2016
+ * Nombre Categoria Descripcion Lista de categorías
+ * 
+ * @author Eisabet Martin Muriel
+ * @version
  *
  */
 public class Categoria {
-	
-	private int id_categoria;			//identificador categoria
-	
-	private String cat_nombre;			//nombre de la categoria
-	
-	private String cat_descripcion;		//descripcion de la categoria
-	
-	
-	public Categoria(){}
-	
-	
-	public boolean isValid(){	
-		return !Validator.isVacio(cat_nombre) &&
-				id_categoria > 0;
+
+	private static final String Throw = null;
+
+	private int id_categoria; // identificador categoria
+
+	private String cat_nombre; // nombre de la categoria
+
+	private String cat_descripcion; // descripcion de la categoria
+
+	public Categoria() {
 	}
-	
+
+	public boolean isValid() {
+		return !Validator.isVacio(cat_nombre) && id_categoria > 0;
+	}
+
 	/**
 	 * Getter para identificador de categoria
+	 * 
 	 * @return Integer con el id de la categoria
 	 */
 	public int getId_categoria() {
 		return id_categoria;
 	}
-	
+
 	/**
 	 * Setter para identificador de categoria
 	 * 
@@ -42,39 +44,53 @@ public class Categoria {
 	public void setId_categoria(int id_categoria) {
 		this.id_categoria = id_categoria;
 	}
-	
+
 	/**
 	 * Getter para el nombre de categoria
+	 * 
 	 * @return cadena con el nombre de la categoria
 	 */
 	public String getCat_nombre() {
 		return cat_nombre;
 	}
-	
+
 	/**
-	 * Setter para el nombre de categoria
+	 * Setter para el nombre de categoria. Debe tener más de cinco y menos de 50
+	 * caracteres
 	 * 
 	 */
-	public void setCat_nombre(String cat_nombre) {
-		this.cat_nombre = cat_nombre;
+	public void setCat_nombre(String cat_nombre) throws DomainException {
+
+		if (Validator.cumpleLongitud(cat_nombre, 6, 49)) {
+			this.cat_nombre = cat_nombre;
+		} else {
+			throw new DomainException("Longitud no Válida para Descripción");
+		}
+
 	}
-	
+
 	/**
 	 * Getter para la descripcion de categoria
+	 * 
 	 * @return cadena con la descripcion de la categoria
 	 */
 	public String getCat_descripcion() {
 		return cat_descripcion;
 	}
-	
+
 	/**
 	 * setter para la descripcion de categoria
 	 * 
 	 */
-	public void setCat_descripcion(String cat_descripcion) {
-		this.cat_descripcion = cat_descripcion;
-	}
+	public void setCat_descripcion(String cat_descripcion) throws DomainException {
 
+		if (Validator.cumpleLongitud(cat_descripcion, 0, 200)) {
+			this.cat_descripcion = cat_descripcion;
+		} else {
+			throw new DomainException("Longitud no Válida para Descripción");
+		}
+
+	}
 
 	@Override
 	public int hashCode() {
@@ -85,7 +101,6 @@ public class Categoria {
 		result = prime * result + id_categoria;
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -111,12 +126,10 @@ public class Categoria {
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Categoria [id_categoria=" + id_categoria + ", cat_nombre=" + cat_nombre + ", cat_descripcion="
 				+ cat_descripcion + "]";
 	}
-	
-	
+
 }
--- Tienda/src/es/rf/tienda/exception/DomainException.java
+++ Tienda/src/es/rf/tienda/exception/DomainException.java
@@ -2,9 +2,18 @@ package es.rf.tienda.exception;
 
 @SuppressWarnings("serial")
 public class DomainException extends Exception {
-	
-	
-	
-	
+	private String message;
+
+	public DomainException() {
+
+	}
+
+	public DomainException(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
 
 }
